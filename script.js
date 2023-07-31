8486a0a860525e3e28c5f9086faa7fb4d8527d31
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-07-31 11:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function name(params) {
+	for(let i=0;i<param.length();i++){
+		if(param.indexOf(param.charAt(i))==param.lastIndexOf(param.charAt())){
+			alert(i);
+			break;
+		}
+	}
+}
+
+ promt("Enter a String");
+alert(name(str));
